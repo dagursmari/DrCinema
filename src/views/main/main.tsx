9b5526041e9296097ca6788b5f35ae5fa62f66c6
arnarmani11 <arnarmani03@gmail.com>
2025-12-08 14:58:52 +0000

    bla

--- src/views/main/main.tsx
+++ src/views/main/main.tsx
@@ -1,17 +1,25 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import styles from "./styles";
+import { useRouter } from "expo-router"
 
-
+const handlePress = () => {
+    router.push("/home-screen")
+};
 
 export function Main() {
+    const router = useRouter();
 
+    const handlePress = () => {
+    router.push("/home-screen")
+    };
     return (
         <View style={styles.container}>
             <Text>Text for LOADING SCREEN</Text>
 
             <TouchableOpacity
+                onPress={handlePress}
                 style={styles.button}>
-                <Text style={styles.buttonText}>LOG IN BUTTON</Text>
+                <Text style={styles.buttonText}>Continue</Text>
             </TouchableOpacity>
         </View>
     )
