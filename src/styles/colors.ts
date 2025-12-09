3b919cc4dfa8bb4183c55acbb2b07634b88216b6
bryndisgunnlaugsd <bryndisg24@ru.is>
2025-12-09 17:10:53 +0000

    bætti inn í colors og logo-i í assets

---
+++ src/styles/colors.ts
@@ -0,0 +1,12 @@
+// Colors we are using for the app
+
+// Grayscale
+export const white = "#FFFFFF";
+export const lightGray = "#f5f5f5";
+export const lighterGray = "#FCFCFC"
+export const black = "#000000";
+export const shadow = "#000";
+export const stroke = "#C1C1C1"
+
+// mainPink
+export const mainPink = "#e94560";
