cedc5c48b434864ca666c20b4d4cf5a6a33819de
Arngrimur Arnason <arngrimurarnason@gmail.com>
2025-12-09 14:23:26 +0000

    Changes to homescreen

--- src/views/home-screen/home-screen-view.tsx
+++ src/views/home-screen/home-screen-view.tsx
@@ -1,27 +1,50 @@
-import { cinemas } from "@/assets/images/dummydata";
 import { CinemaSectionComp } from "@/src/components/cinema-section/cinema-section";
 import SearchBarComp from "@/src/components/searchbar/searchbar";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import styles from "./styles";
 
 
-export function HomeScreenView() {
+export function HomeScreenView({ movies }) {
 
-    const [filteredCinemas, setFilteredCinemas] = useState(cinemas);
+    const [filteredCinemas, setFilteredCinemas] = useState([]);
+    const [cinemas, setCinemas] = useState([]);
+
+    useEffect(() => {
+    if (!movies?.length) return;
+
+    const cinemasMap = {};
+    movies.forEach((movie) => {
+      movie.showtimes?.forEach((showtime) => {
+        const cinemaId = showtime.cinema.id;
+        if (!cinemasMap[cinemaId]) {
+          cinemasMap[cinemaId] = {
+            id: cinemaId,
+            name: showtime.cinema.name,
+            movies: [],
+          };
+        }
+        cinemasMap[cinemaId].movies.push(movie);
+      });
+    });
+
+
+    const cinemasArray = Object.values(cinemasMap);
+        setCinemas(cinemasArray);
+        setFilteredCinemas(cinemasArray);
+    }, [movies]);
 
     const handleSearch = (text: string) => {
 
     if (text.trim() === "") {
-      setFilteredCinemas(cinemas); // reset
+      setShowtimes(movie.showtimes); // reset
       return;
     }
 
     const lower = text.toLowerCase();
 
-    // Filter inside each cinema
     const filtered = cinemas
       .map((cinema) => ({
         ...cinema,
@@ -29,21 +52,25 @@ export function HomeScreenView() {
           movie.title.toLowerCase().includes(lower)
         ),
       }))
-      // Remove cinemas with NO matching movies
       .filter((cinema) => cinema.movies.length > 0);
+  };    
+    
+    type CinemaWithMovies = {
+        cinema: { id: number; name: string };
+        movies: { movie: any; showtime: any }[];
+    };
 
-    setFilteredCinemas(filtered);
-  };
+
     return( 
         <SafeAreaView style={styles.container} edges={['left', 'right', 'bottom']}>
       <View>
         <SearchBarComp onSearch={handleSearch} />
       </View>
 
-      <FlatList
+      <FlatList<CinemaWithMovies>
         data={filteredCinemas}
-        renderItem={({ item }) => <CinemaSectionComp cinema={item} />}
-        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <CinemaSectionComp cinema={item}/>}
+        keyExtractor={(item) => item.cinema.id.toString()}
         style={styles.list}
         contentContainerStyle={{ paddingTop: 0 }}
       />
