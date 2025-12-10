0645a8d4563f250e1a42ccc983a23f668a4dbacf
dagursmari <dagurss24@ru.is>
2025-12-10 10:19:56 +0000

    comments and small changes to api

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 
-// Import all the reducers (warehouse workers)
-import moviesReducer from './slices/movies-slice';
-import favoritesReducer from './slices/favorites-slice';
+// Import all the reducers
+import moviesReducer from "./slices/movies-slice";
+import favoritesReducer from "./slices/favorites-slice";
 import cinemasReducer from "./slices/cinemas-slice"
-// TODO: Import cinemasReducer, upcomingReducer, genresReducer when you create them
+// TODO: Import upcomingReducer, genresReducer
 
-/**
- * This is the STORE - the main warehouse
- * It combines all the slices into one place
- */
+//This is the STORE - the main warehouse
+//It combines all the slices into one place
+
 export const store = configureStore({
   reducer: {
     movies: moviesReducer,       // Movies shelf
